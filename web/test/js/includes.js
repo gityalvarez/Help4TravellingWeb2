d57924b67a680f05f993bc86378cee57713f5e88
Santiago <Santiago@192.168.56.1>
2016-09-24 14:11:07 -0300

    header y footer separados

---
+++ web/test/js/includes.js
@@ -0,0 +1,12 @@
+
+//Espera a que el documento esta totalmente cargado
+$(document).ready(function () {
+    
+    //Carga el cabezal
+    $("#header").load("header.html");
+    
+    //Carga el footer
+    $("#footer").load("footer.html");
+});
+
+
